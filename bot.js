cbb4691473cb1143ba78271e713562666683d13a
Mustafa Muse <umpp101@icloud.com>
2020-04-26 23:17:33 -0700

    i added a botStatusChecker, because it was timing out, also  split up the functions for debugging

--- bot.js
+++ bot.js
@@ -65,23 +65,6 @@ const searchTerms = (keywordsArray) => {
   return keywordsArray[randomIndex];
 };
 
-// console.log(`Theses are my ${searchTerms(keywords)}`);
-
-myBot.post(
-  "statuses/update",
-  {
-    status: "We are aliveeee again!",
-  },
-  (err, data, response) => {
-    if (err) {
-      console.log(`YOU GOT THIS ERROR: ${err.message}`);
-    } else {
-      // console.log(data)
-      //   console.log(`${data.text} tweeted!`)
-    }
-  }
-);
-
 
 retweetJobPosts = () => {
   //create a function to call on with the set Interval
@@ -95,25 +78,74 @@ retweetJobPosts = () => {
 };
 
 searchedTweets = (params) => {
-    myBot.get("search/tweets", params, (err, data) => {
-        if (err) {
-            console.log(`YOU GOT THIS ERROR: ${err.message}`);
-        } else {
-            let tweets = data.statuses; // grab recent tweets
-            // console.log(tweets)
-            tweets.forEach((tweet) => {
-                let retweetId = tweet.id_str; // grad the id string to use it to retweet
-                // console.log(retweetId)
-                postNewTweets(retweetId); // call on the function that will retweet the new job tweets
-            });
-        }
-    });
-}
+  myBot.get("search/tweets", params, (err, data) => {
+    if (err) {
+      console.log(`YOU GOT THIS ERROR: ${err.message}`);
+    } else {
+      let tweets = data.statuses; // grab recent tweets
+      // console.log(tweets)
+      tweets.forEach((tweet) => {
+        let retweetId = tweet.id_str; // grab the id string to use it to retweet
+        // console.log(retweetId)
+        postNewTweets(retweetId); // call on the function that will retweet the new job tweets
+      });
+    }
+  });
+};
 
 postNewTweets = (retweetId) => {
-    myBot.post("statuses/retweet/:id", { id: retweetId }, (err, data) => {
-        data ? console.log("found one and retweeted it! ") : console.log(err);
-    });
+  myBot.post("statuses/retweet/:id", { id: retweetId }, (err, data) => {
+    data ? console.log("found one and retweeted it! ") : console.log(`YOU GOT THIS ERROR: ${err.message}`);
+  });
+};
+
+
+
+
+//This is to see if my bot is down or not
+let botIsDown = false;
+
+botStatusCheck = (err) => {
+  // Check if our tweet has been successful, if we've reached our rate limit, let people know that our bot is asleep.
+  if (err !== undefined) {
+    console.log(err);
+    if (err.code === 88) {
+      // meaning i exceeded my rate limit
+      imDown();
+      botIsDown = true;
+    }
+  } else {
+    if (botIsDown) {
+      botIsDown = false;
+      liveAgain();
+    }
+  }
+};
+
+function imDown() {
+  myBot.post(
+    "account/update_profile",
+    {
+      name: "Junior Dev Jobs",
+      description:
+        "They caught on to me today,i guess i reached my rate limit. See you tomorrow",
+    },
+    botStatusCheck
+  );
+}
+function liveAgain() {
+  myBot.post(
+    "account/update_profile",
+    {
+      name: "Junior Dev Bot",
+      description: "Out here doing to make sure you get your chance",
+    },
+    botStatusCheck
+  );
 }
 
-setInterval(retweetJobPosts, 15000);
+tasks = () => {
+  retweetJobPosts(), botStatusCheck();
+};
+
+setInterval(tasks, 15000);
