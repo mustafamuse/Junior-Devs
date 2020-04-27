b9da31a70d1535ef966be165e558bbadd31b80bb
Mustafa Muse <umpp101@icloud.com>
2020-04-26 17:10:21 -0700

    posting works

--- bot.js
+++ bot.js
@@ -1,5 +1,18 @@
+
 const Twit = require('twit')
 const config = require('./config')
 
-const Twitter = new Twit(config);
 
+
+const myBot = new Twit(config);
+
+myBot.post('statuses/update', {
+    status: 'We Are Live Babyyyyyy!'
+  }, (err, data, response) => {
+    if (err) {
+      console.log(err)
+    } else {
+        console.log(data)
+      console.log(`${data.text} tweeted!`)
+    }
+  })
